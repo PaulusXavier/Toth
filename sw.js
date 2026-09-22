f1366788194a411581612041e70cb460b9af6443
Paulo Xavier <paulo.ae18@gmail.com>
2026-09-22 16:58:56 -0400

    Update CACHE_NAME to caderno-psi-v25

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 //   propaga sozinha, só que sem atrasar a primeira exibição.
 // ============================================================
 
-const CACHE_NAME = 'caderno-psi-v24';
+const CACHE_NAME = 'caderno-psi-v25';
 const ASSETS = [
   './',
   './index.html',
